dea6a48e8681cde04a601eed030437034d729131
christophebe <christophe.lombart@gmail.com>
2014-07-22 09:50:03 +0200

    add new command create

--- bin/exec.js
+++ bin/exec.js
@@ -2,14 +2,24 @@
 
 var preview  = require('../lib/preview.js');
 var generate = require('../lib/generate.js');
-
+var create = require('../lib/create.js');
 
 if ( process.argv.length != 3 ) {
-   console.log('Usage: cocoons [preview|generate]\n');
+   console.log('Usage: cocoons [create|preview|generate]\n');
    return;
 }
 
 switch (process.argv[2]) {
+      case "create":
+          create.createWebsite(function(error, status) {
+              if (error) {
+                console.log("Error during the creation of the site : " + error );
+              }
+              else {
+                console.log("The site is correctly created : " + status);
+              }
+          });
+          break;
       case "preview":
           preview.preview();
           break;
@@ -24,6 +34,6 @@ switch (process.argv[2]) {
           });
           break;
       default:
-          console.log("Invalid command, use : cocoons [preview|generate]");
+          console.log("Invalid command, use : cocoons [create|preview|generate]");
           break;
 }
