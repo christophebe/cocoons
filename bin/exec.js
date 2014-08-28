83da8d53475da3955d06c2b5b22a9c4921f743d3
christophebe <christophe.lombart@gmail.com>
2014-08-28 21:15:17 +0200

    Add support to deploy the site with FTP

--- bin/exec.js
+++ bin/exec.js
@@ -2,26 +2,27 @@
 
 var preview  = require('../lib/exec/preview.js');
 var generate = require('../lib/exec/generate.js');
-var create = require('../lib/exec/create.js');
-var log = require('../lib/logger').Logger;
+var deploy   = require('../lib/exec/deploy-ftp.js');
+var create   = require('../lib/exec/create.js');
+var log      = require('../lib/logger').Logger;
 
 /**
- * Global application used to create, preview & generate a web site
+ * Global application used to create, preview,generate & ftp (deploy) a web site
  *
  */
 if ( process.argv.length < 3 ) {
-   console.log('Usage: cocoons [create|preview|generate]\n');
+   console.log('Usage: cocoons [create|preview|generate|ftp]\n');
    return;
 }
 
 switch (process.argv[2]) {
       case "create":
           var siteTemplateName;
-          
+
           if (process.argv.length == 4) {
             siteTemplateName = process.argv[3];
           }
-          create.createWebsite(siteTemplateName, function(error, status) {
+          create.createSite(siteTemplateName, function(error, status) {
               if (error) {
                 log.error("Error during the creation of the site : " + error );
               }
@@ -37,11 +38,11 @@ switch (process.argv[2]) {
           if (process.argv.length == 4) {
             projectFolder = process.argv[3];
           }
-          preview.preview(projectFolder);
+          preview.previewSite(projectFolder);
           break;
 
       case "generate":
-          generate.generateStaticSite(function(error, targetFolder) {
+          generate.generateSite(function(error, targetFolder) {
               if (error) {
                 log.error("Error during the generation of the site : " + error );
               }
@@ -51,7 +52,18 @@ switch (process.argv[2]) {
           });
           break;
 
+      case "ftp" :
+          deploy.deploySite(function(error, serverInfo){
+              if (error) {
+                log.error("Error during the deployment of the site : " + error );
+              }
+              else {
+                log.info("The site is correctly deployed in " + serverInfo);
+              }
+          });
+          break;
+
       default:
-          log.error("Invalid command, use : cocoons [create|preview|generate]");
+          log.error("Invalid command, use : cocoons [create|preview|generate|deploy]");
           break;
 }
