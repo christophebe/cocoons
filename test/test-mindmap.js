c70be04c73aaf591044473580ba75f8a71c26abf
christophebe <christophe.lombart@gmail.com>
2016-01-13 23:22:31 +0100

    review how to generate the site from a mindmap

--- test/test-mindmap.js
+++ test/test-mindmap.js
@@ -5,15 +5,17 @@ var m      = require("../lib/fs/mindmap-fs.js");
 
 describe('Test Mindmap', function(){
 
-      it('Generate a site from a mindmap', function(done) {
+      it('Generate a site from a mindmap - opml file', function(done) {
           var mindmapFilePath = "test/site-test-mindmap/test.opml";
 
           var config =  {
             mindmapConverter : "../mindmap/opml-converter.js",
             source : "src",
             templateFolder : "templates",
-            mindmapPrefix : "gg/content"
-          }
+            mindmapPrefix : "gg/content",
+            country : "be"
+
+          };
 
           var mindmap = new m.Mindmap(config, "test/site-test-mindmap");
 
@@ -28,9 +30,34 @@ describe('Test Mindmap', function(){
               }
 
           });
-
-
-
      });
+
+     it('Generate a site from a mindmap - freemind file', function(done) {
+         var mindmapFilePath = "test/site-test-mindmap/pret.mm";
+
+         var config =  {
+           mindmapConverter : "../mindmap/freemind-converter.js",
+           source : "src",
+           templateFolder : "templates",
+           mindmapMardownTemplate : "mindmap/markdown-fr.jade",
+           mindmapJsonTemplate : "mindmap/json-fr.jade",
+           mindmapPrefix : "gg/content",
+           country : "be"
+         };
+
+         var mindmap = new m.Mindmap(config, "test/site-test-mindmap");
+
+         mindmap.generateFromMindmap(mindmapFilePath, function(error, folder) {
+             if (error) {
+                 done(error);
+             }
+             else {
+                 fs.exists('test/site-test-mindmap/gg/content/pret-voiture-moto/moto.md', function (exists) {
+                   done();
+                 });
+             }
+
+         });
+    });
 
 });
